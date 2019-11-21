49770e22d3ece35aa3e2d4013f19c3e5b12e030d
Daniil_Spirin <daniil_spirin@epam.com>
2019-11-21 09:45:39 +0300

    test added

---
+++ src/test/java/random/RandomTest.java
@@ -0,0 +1,12 @@
+package random;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class RandomTest
+{
+    @Test
+    public void testTrue() {
+        assertTrue(true);
+    }
+}
